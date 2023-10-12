fa88bac6b09c00836acfb76f1a834da7fb749111
Rhilim <arhilim@gmail.com>
2023-10-12 03:26:30 -0500

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,62 @@
-const key = new Key();
+class Key {
+  private signature: number;
 
-const house = new MyHouse(key);
-const person = new Person(key);
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+  protected key: Key;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("The door is open. The person has entered the house.");
+    } else {
+      console.log("The door is closed. The person cannot enter the house.");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  constructor() {
+    super();
+    this.key = new Key();
+  }
+
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("The door is open.");
+    } else {
+      console.log("The door remains closed. Incorrect key.");
+    }
+  }
+}
+
+const house = new MyHouse();
+const person = new Person(new Key());
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
